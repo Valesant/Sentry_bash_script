455ec3064a1294c3d793a8b8ee6364e31d5ed4c2
Valesant <46745584+Valesant@users.noreply.github.com>
2024-04-02 13:06:34 +0200

    Update fancy_suggestions.sh

--- fancy_suggestions.sh
+++ fancy_suggestions.sh
@@ -15,44 +15,37 @@ for tool in jq bc; do
     fi
 done
 
-# Fetch suggestions
 suggestionsResponse=$(curl -s -X GET "$apiUrl/suggestions?addresses=$address" -H "Authorization: $apiKey")
 
-# Fetch tokens directly in the wallet
+# Tokens in Wallet
 echo "Available metrics:"
 echo "----------"
 echo "On tokens in Wallet:"
 walletTokens=$(echo "$suggestionsResponse" | jq -r '.data.metrics[] | select(.info.pool == null) | .info.token.address' | uniq)
 for token in $walletTokens; do
-    tokenMetrics=$(echo "$suggestionsResponse" | jq --arg token "$token" -r '.data.metrics[] | select(.info.token.address == $token and .info.pool == null)')
-    tokenName=$(echo "$tokenMetrics" | jq -r '.info.token.name' | uniq)
-    metricKeys=$(echo "$tokenMetrics" | jq -r '.name' | paste -sd ";" -)
-
-    echo "$tokenName"
-    echo "$metricKeys" | tr ';' '\n'
+    tokenName=$(echo "$suggestionsResponse" | jq -r --arg token "$token" '.data.metrics[] | select(.info.token.address == $token) | .info.token.name' | head -1)
+    usdAmount=$(echo "$suggestionsResponse" | jq -r --arg token "$token" '.data.supportedAssets[] | select(.tokenAddress == $token) | .usdAmount' | head -1)
+    echo "$tokenName usdAmount: $usdAmount"
+    echo "$suggestionsResponse" | jq -r --arg token "$token" '.data.metrics[] | select(.info.token.address == $token) | .name'
     echo "-"
 done
 
 echo "----------"
 echo "Other available tokens:"
-# Construct a list of all unique pool token addresses not already listed
-allPoolTokenAddresses=$(echo "$suggestionsResponse" | jq -r '.data.metrics[] | select(.info.pool != null) | .info.pool.tokenAddresses[]' | uniq)
-for token in $allPoolTokenAddresses; do
-    if ! [[ $walletTokens =~ $token ]]; then
-        # Secondary API call for tokens details
-        tokenDetails=$(curl -s -X GET "$apiUrl/tokens?addresses=$token" -H "accept: application/json")
-        echo "$tokenDetails" | jq -r '.data[] | select(.isTracked == true) | "\(.name) (\(.symbol))\n\(.symbol) total supply\n\(.symbol) total tvl\n-"'
-    fi
+# Other available tokens in pools (Unique tokens not in wallet)
+poolTokenAddresses=$(echo "$suggestionsResponse" | jq -r '.data.metrics[] | select(.info.pool != null) | .info.pool.tokenAddresses[]' | uniq | grep -v -f <(echo "$walletTokens"))
+for token in $poolTokenAddresses; do
+    tokenDetails=$(curl -s -X GET "$apiUrl/tokens?addresses=$token" -H "accept: application/json")
+    echo "$tokenDetails" | jq -r '.data[] | select(.isTracked == true) | "\(.name) (\(.symbol))\n\(.symbol) total supply\n\(.symbol) total tvl\n-"'
 done
 
 echo "----------"
 echo "On positions:"
-# Assuming pool names or addresses can be directly extracted and are unique
-poolNames=$(echo "$suggestionsResponse" | jq -r '.data.metrics[] | select(.info.pool != null) | .info.pool.name' | uniq)
-for poolName in $poolNames; do
+# Group metrics by pool
+poolAddresses=$(echo "$suggestionsResponse" | jq -r '.data.metrics[] | select(.info.pool != null) | .info.pool.address' | uniq)
+for poolAddress in $poolAddresses; do
+    poolName=$(echo "$suggestionsResponse" | jq -r --arg poolAddress "$poolAddress" '.data.metrics[] | select(.info.pool.address == $poolAddress) | .info.pool.name' | head -1)
     echo "$poolName:"
-    poolMetrics=$(echo "$suggestionsResponse" | jq --arg poolName "$poolName" -r '.data.metrics[] | select(.info.pool.name == $poolName)')
-    metricKeys=$(echo "$poolMetrics" | jq -r '.name')
-    echo "$metricKeys"
+    echo "$suggestionsResponse" | jq -r --arg poolAddress "$poolAddress" '.data.metrics[] | select(.info.pool.address == $poolAddress) | .name'
     echo "-"
 done
